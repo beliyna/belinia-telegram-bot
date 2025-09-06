df053964cdaa956b8158bbe78abb7bfda71c8014
beliyna <belinaydlb@gmail.com>
2025-09-06 13:56:53 +0300

    index.js

--- index.js
+++ index.js
@@ -2,26 +2,20 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-if (!BOT_TOKEN) throw new Error('BOT_TOKEN bulunamadı. .env dosyasını kontrol et.');
+if (!BOT_TOKEN) throw new Error('BOT_TOKEN bulunamadı.');
 
 const bot = new Telegraf(BOT_TOKEN);
 
 // Komutlar
 bot.start(ctx => ctx.reply('Merhaba! Bot çalışıyor 🚀'));
 bot.hears(/selam/i, ctx => ctx.reply('selam!'));
-bot.on('text', ctx => {
-  const text = ctx.message.text.toLowerCase();
-  if (text.includes('patron') || text.includes('admin')) {
-    return ctx.reply('tek sahibim beliyna');
-  }
-  ctx.reply('mesajını aldım');
-});
+bot.hears(/patron|admin/i, ctx => ctx.reply('tek sahibim beliyna'));
+bot.on('text', ctx => ctx.reply('mesajını aldım'));
 
 // Botu başlat
-bot.launch().then(() => {
-  console.log('Bot çalışmaya başladı...');
-});
+bot.launch().then(() => console.log('Bot çalışıyor'));
 
-// Windows kapanırken düzgün dursun
+// Kapatma durumları
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
